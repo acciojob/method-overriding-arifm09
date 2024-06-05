f2072a66e69f33c850d13a65f1d0a3158d3f1a17
Mohd Arif <arifm0949@gmail.com>
2024-06-06 00:39:10 +0530

    task 1,2,3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
+class A{
+    void meth(){
+        System.out.println("Invoking method from class A");
+    }
+}
+class B extends A{
 
+}
 public class Main {
-  
+    public static void main(String[] args) {
+          B obj = new B();
+          obj.meth();
+    }
 }
